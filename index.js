523c3d3f2b27c971cdb2c21ad2f6db19d1704d78
Krupakokila <krupa.kokila@thebluspire.com>
2024-11-14 10:42:28 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+//read item
+app.get("/", (req, res) => {
+  console.log("get request successful");
+  res.send("success!!");
+});
+
+app.listen(PORT, () =>
+  console.log(`server running on http://localhost:${PORT}`)
+);
+
+module.exports = app;
